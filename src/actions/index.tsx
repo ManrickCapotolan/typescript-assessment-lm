f057fb78e41383cd2e2920bcf73cc141c532dc44
ManrickCapotolan <manrickcapotolan@gmail.com>
2020-07-23 19:41:54 +0800

    add types

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,1 +1,24 @@
+import { ActionCreatorsMapObject } from 'redux';
 import { AppEvents } from "../events";
+
+export interface IActionWithPayload<T extends string, P> {
+  type: T;
+  payload: P;
+}
+export function createAction<T extends string, P>(type: T, payload: P): IActionWithPayload<T, P> {
+  return { type, payload };
+}
+
+export const SetName = (name: string) => createAction(AppEvents.SET_NAME, name);
+export const SetAge = (age: number) => createAction(AppEvents.SET_AGE, age);
+
+export const Actions = {
+  SetAge,
+  SetName,
+}
+
+export type IActionsUnion<A extends ActionCreatorsMapObject> = ReturnType<
+  A[keyof A]
+>;
+
+export type IAction = IActionsUnion<typeof Actions>;
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,2 +1,33 @@
 import { AppEvents } from "../events";
 import { IAction } from "../actions";
+
+export interface IUserState {
+  name: string;
+  age: number;
+}
+
+const initialState: IUserState = {
+  name: '',
+  age: 0,
+};
+
+const reducer = (state = initialState, action: IAction): IUserState => {
+  switch (action.type) {
+    case AppEvents.SET_NAME: {
+      return {
+        ...state,
+        name: action.payload,
+      }
+    }
+    case AppEvents.SET_AGE: {
+      return {
+        ...state,
+        age: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export { reducer };
